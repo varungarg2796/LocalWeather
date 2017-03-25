7dd15575d038b11f26a43b6226aa6a1cb2816311
varungarg2796 <varungarg2796@hotmail.com>
2017-03-25 23:07:19 +0530

    location extraction

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //Some javascript will come here
+//
+//
+//
+var lati;
+var longi;
+window.onload=function ()
+{
+	getLocation();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    lati= position.coords.latitude;
+    longi= position.coords.longitude;
+    console.log(lati);
+    console.log(losngi);
+}
