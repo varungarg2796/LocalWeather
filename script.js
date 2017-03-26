6e08e282e3e44554d32946c1494cc1bdcbe0e056
varungarg2796 <varungarg2796@hotmail.com>
2017-03-26 19:36:00 +0530

    Added more functionality

--- script.js
+++ script.js
@@ -4,14 +4,37 @@
 //
 var lati;
 var longi;
-
+//api key - 2d11371484be072a931fa3ec4815f349
+//http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=2d11371484be072a931fa3ec4815f349
+//json request from above.
 //do this to get only the city name from search bar.
-var stri= "Varun, is bad";
+var stri= "Varun was, is bad";
 var heythere= stri.indexOf(',');
 console.log(heythere);
 console.log(stri.slice(0,heythere));
 
+function getCityWeather()
+{	var cityEntered= $("#autocomplete").val();
+	if (cityEntered=="")
+	{
+		alert("Please enter a city");
+	}
+	else 
+	{
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+cityEntered+"&appid=2d11371484be072a931fa3ec4815f349", function (data) {
+		            var rawJson = JSON.stringify(data);
+		            var json = JSON.parse(rawJson);
+		            console.log(json);
+		            showWeather(json); //Update Weather parameters
+		        });
+	}
 
+}
+
+function showWeather(json)
+{
+	$("#displayWeather").text(json.main.temp);
+}
 
 var input = document.getElementById('autocomplete');
       var autocomplete = new google.maps.places.Autocomplete(input);
