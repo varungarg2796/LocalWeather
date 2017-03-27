f4858ef0b391a3055455b350cd9d7bbc5355d714
varungarg2796 <varungarg2796@hotmail.com>
2017-03-28 01:43:08 +0530

    Added more functionality

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-//Some javascript will come here
-//
-//
-//
-var lati;
-var longi;
 //api key - 2d11371484be072a931fa3ec4815f349
 //http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=2d11371484be072a931fa3ec4815f349
 //json request from above.
 //do this to get only the city name from search bar.
-var stri= "Varun was, is bad";
-var heythere= stri.indexOf(',');
-console.log(heythere);
-console.log(stri.slice(0,heythere));
-var skycons = new Skycons({"color": "white"});
 //skycons.add(document.getElementById("icon2"), Skycons.RAIN);
-//skycons.play();
-//
+
+
+
+
+var lati,long,timeHour,timeFull,weather,temp;
+
+var skycons = new Skycons({"color": "white"});
+skycons.add("setSkycon", Skycons.CLEAR_DAY);
+skycons.play();
+
 function getTimeOfCity(json)
 {
 	longi = json.coord.lon;
 	lati = json.coord.lat;
-	$.getJSON('http://api.geonames.org/timezoneJSON?lat=' + latitude + '&lng=' + longitude + '&username=varungarg2796',
+	$.getJSON('http://api.geonames.org/timezoneJSON?lat=' + lati + '&lng=' + longi + '&username=varungarg2796',
 	 function(time) {
 			var rawTimeZone = JSON.stringify(time);
 			var parsedTimeZone = JSON.parse(rawTimeZone);
 			var dateTime = parsedTimeZone.time;
 			timeFull = dateTime.substr(11);
-			$(".local-time").html(timeFull); //Update local time
+			$("#timePos").html(timeFull); //Update local time
 			timeHour = dateTime.substr(-5, 2);
 	});
 }
@@ -47,6 +44,7 @@ function getCityWeather()
 		            var rawJson = JSON.stringify(data);
 		            var json = JSON.parse(rawJson);
 		            console.log(json);
+		            getTimeOfCity(json);
 		            showWeather(json); //Update Weather parameters
 		 });
 	}
@@ -55,15 +53,94 @@ function getCityWeather()
 
 function showWeather(json)
 {
-	$("#displayWeather").text(json.main.temp);
-	var weatherType= json.weather[0].description;
+	weather= json.weather[0].description;
+	temp = (json.main.temp - 273.15).toFixed(0) + "°C",
+	
+	$("#tempPos").text(temp); //displays the temp
+	$("#weatherConditionPos").html(json.weather[0].description); //displays the weather condition
+	console.log(weather);
+
+		if(weather.indexOf("rain") >= 0) {
+			console.log('rain');
+			skycons.set("setSkycon", Skycons.RAIN);
+		}
+
+		else if (weather.indexOf("sunny") >= 0) {
+			console.log('sun');
+			skycons.set("setSkycon", Skycons.CLEAR_DAY);
+		}
+
+		else if (weather.indexOf("clear") >= 0) {
+			if (timeHour >= 7 && timeHour < 20) {
+				console.log('clear day');
+				skycons.set("setSkycon", Skycons.CLEAR_DAY);
+			}
+
+			else {
+				console.log('clear night');		
+				skycons.set("setSkycon", Skycons.CLEAR_NIGHT);
+			}		
+		}
+
+		else if (weather.indexOf("cloud") >= 0) {
+			if (timeHour >= 7 && timeHour < 20) {
+
+				console.log('cloudy');
+				skycons.set("setSkycon", Skycons.PARTLY_CLOUDY_DAY);
+			}
+
+			else {
+				console.log('cloudy');
+				skycons.set("setSkycon", Skycons.PARTLY_CLOUDY_NIGHT);
+			}	
+		}
+
+		else if (weather.indexOf("thunderstorm") >= 0) {
+				console.log('thunderstorm');
+			skycons.set("setSkycon", Skycons.SLEET);
+		}
+
+		else if (weather.indexOf("snow") >= 0) {
+			console.log('thunderstorm');
+			skycons.set("setSkycon", Skycons.SNOW);
+		}
+		else if(weather.indexOf("haze")>=0)
+		{
+			console.log("haze");
+			skycons.set("setSkycon", Skycons.FOG);	
+		}
+		else if(weather.indexOf("wind")>=0)
+		{
+			console.log("wind");
+			skycons.set("setSkycon", Skycons.WIND);	
+		}
+		else
+		{
+			alert("Skycon not present")
+		}
 }
 
+
+
+
+
+
+
+
+
+
 var input = document.getElementById('autocomplete');
-      var autocomplete = new google.maps.places.Autocomplete(input);
+var autocomplete = new google.maps.places.Autocomplete(input);
+
+
+
+
+
+
+//later
 window.onload=function ()
-{
-	getLocation();
+{	//getLocation();
+	
 }
 
 function getLocation() {
