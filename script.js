042606352ad8913aa89be04db919f51e65387265
varungarg2796 <varungarg2796@hotmail.com>
2017-03-27 23:56:54 +0530

    Added more functionality

--- script.js
+++ script.js
@@ -13,24 +13,42 @@ var heythere= stri.indexOf(',');
 console.log(heythere);
 console.log(stri.slice(0,heythere));
 var skycons = new Skycons({"color": "white"});
-skycons.add(document.getElementById("icon2"), Skycons.RAIN);
-skycons.play();
+//skycons.add(document.getElementById("icon2"), Skycons.RAIN);
+//skycons.play();
+//
+function getTimeOfCity(json)
+{
+	longi = json.coord.lon;
+	lati = json.coord.lat;
+	$.getJSON('http://api.geonames.org/timezoneJSON?lat=' + latitude + '&lng=' + longitude + '&username=varungarg2796',
+	 function(time) {
+			var rawTimeZone = JSON.stringify(time);
+			var parsedTimeZone = JSON.parse(rawTimeZone);
+			var dateTime = parsedTimeZone.time;
+			timeFull = dateTime.substr(11);
+			$(".local-time").html(timeFull); //Update local time
+			timeHour = dateTime.substr(-5, 2);
+	});
+}
 
 
 function getCityWeather()
-{	var cityEntered= $("#autocomplete").val();
+{	
+	var cityEntered= $("#autocomplete").val();
 	if (cityEntered=="")
 	{
 		alert("Please enter a city");
 	}
 	else 
 	{
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+cityEntered+"&appid=2d11371484be072a931fa3ec4815f349", function (data) {
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+cityEntered+"&appid=2d11371484be072a931fa3ec4815f349",
+		 function (data) 
+		 {
 		            var rawJson = JSON.stringify(data);
 		            var json = JSON.parse(rawJson);
 		            console.log(json);
 		            showWeather(json); //Update Weather parameters
-		        });
+		 });
 	}
 
 }
@@ -38,6 +56,7 @@ function getCityWeather()
 function showWeather(json)
 {
 	$("#displayWeather").text(json.main.temp);
+	var weatherType= json.weather[0].description;
 }
 
 var input = document.getElementById('autocomplete');
